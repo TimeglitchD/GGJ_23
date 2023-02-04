818dd247382682cfe4b6645dedb762ee7ec68a16
Leroy Freeth <ldfreeth@hotmail.com>
2023-02-04 03:27:41 +0100

    Added a turn counter

---
+++ Assets/Scripts/Core/TurnCounter.cs
@@ -0,0 +1,44 @@
+using Synapse.Controls;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Synapse.Core
+{
+    public class TurnCounter : MonoBehaviour
+    {
+        int turnCounter = 0;
+        [SerializeField] private NodeConnectionsInput connector;
+
+        public UnityEvent<int> OnNextTurn;
+
+#if UNITY_EDITOR
+        [Space]
+        [Header("Debug")]
+        [SerializeField] private bool ShowDebugLogs = false;
+#endif
+
+        private void Start()
+        {
+            connector.BeforeConnected.AddListener(NextTurn);
+        }
+
+        private void NextTurn()
+        {
+            turnCounter++;
+            OnNextTurn?.Invoke(turnCounter);
+#if UNITY_EDITOR
+            Debug.Log($"Turn counter: {turnCounter}");
+#endif
+        }
+
+        private void Reset()
+        {
+            connector = FindAnyObjectByType<NodeConnectionsInput>();
+            if (connector == null)
+            {
+                Debug.LogError($"{typeof(TurnCounter)} requires a {typeof(NodeConnectionsInput)} to be present in the scene.");
+                DestroyImmediate(this);
+            }
+        }
+    }
+}
